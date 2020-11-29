96f770f92f3de4b6bef3e99db9910787186d1e4c
Raghav Nakra <71371427+ragh-a-void@users.noreply.github.com>
2020-11-29 12:14:27 +0530

    Delete TheButton.java

--- src/model/TheButton.java
+++
@@ -1,9 +0,0 @@
-package model;
-
-import javafx.scene.control.Button;
-
-public class TheButton extends Button
-{
-    private final String Font_Path = "src/model/resources/kenvector_future.ttf";
-//    private final String Button_pressed_style = "-fx-background-"
-}
